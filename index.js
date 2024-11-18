df616017ab013f07ef1c46cd5cb8a04b2c8592d1
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-18 19:25:54 +0100

    7. Add an event listener to the closeTaskFormBtn variable and pass in a click event for the first argument and a callback function for the second argument. For the callback function, call the showModal() method on the confirmCloseDialog element. This will display a modal with the Discard and Cancel buttons.

--- index.js
+++ index.js
@@ -15,3 +15,7 @@ let currentTask = {};
 openTaskFormBtn.addEventListener("click", () => {
   taskForm.classList.toggle("hidden");
 });
+
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
