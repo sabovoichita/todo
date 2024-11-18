63d9a210dbda28ae0001102f4f637c1457372fad
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-18 19:10:41 +0100

    1. Begin by accessing the task-form, confirm-close-dialog, and open-task-form-btn elements with the getElementById() method. Save them in the variables taskForm, confirmCloseDialog, and openTaskFormBtn.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("WElcome!");
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
