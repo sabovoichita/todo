4a89f7b01a871e82bd76165644f0c1ff6a0c0b38
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-19 13:38:55 +0100

    64. Check if there's a task inside taskData, then call the updateTaskContainer() inside the if statement block.

--- index.js
+++ index.js
@@ -82,6 +82,10 @@ const reset = () => {
   currentTask = {};
 };
 
+if (taskData.length) {
+  updateTaskContainer();
+}
+
 openTaskFormBtn.addEventListener("click", () => {
   taskForm.classList.toggle("hidden");
 });
