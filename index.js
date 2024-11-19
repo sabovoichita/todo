9673c303a5f5da3ca4e63accc1f725d5f9fefc96
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-19 13:14:40 +0100

    54. A myTaskArr array has been provided for you. Use the setItem() method to save it with a key of data. After that, open your browser console and go to the Applications tab, select Local Storage, and the freeCodeCamp domain you see.

--- index.js
+++ index.js
@@ -114,3 +114,10 @@ taskForm.addEventListener("submit", (e) => {
 
   addOrUpdateTask();
 });
+
+const myTaskArr = [
+  { task: "Walk the Dog", date: "22-04-2022" },
+  { task: "Read some books", date: "02-11-2023" },
+  { task: "Watch football", date: "10-08-2021" },
+];
+localStorage.setItem("data", myTaskArr);
