4513907bab2b5dd3cc7915bc155443f4c487737f
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-19 11:32:01 +0100

    28. Create two button elements with the type attribute set to button and the class attribute set to btn. Set the text of the first button to Edit and the text of the second button to Delete.

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ taskForm.addEventListener("submit", (e) => {
         <p><strong>Title:</strong>${title}</p>
         <p><strong>Date:</strong> ${date}</p>
         <p><strong>Description:</strong> ${description}</p>
-    </div>`;
+        <button type="button" class="btn">Edit</button>
+        <button type="button" class="btn">Delete</button>
+        </div>`;
   });
 
   //   console.log(taskObj);
