410b01bd090335e160694ecc2672fd8143b3da2c
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-19 10:34:31 +0100

    11. Use const to declare a variable called dataArrIndex and assign it the value of taskData.findIndex(). For the findIndex() method, pass in an arrow function with item as the parameter. Within the arrow function, check if the id property of item is strictly equal to the id property of currentTask.

--- index.js
+++ index.js
@@ -32,3 +32,5 @@ discardBtn.addEventListener("click", () => {
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 });
+
+const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
