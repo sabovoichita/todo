b9fbc97cb9cfaaf62da17581cfdbf4da961bf0f6
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-19 12:03:51 +0100

    37. Use arrow syntax to create an updateTaskContainer function. Then move the taskData.forEach() and its content into it.

--- index.js
+++ index.js
@@ -21,9 +21,24 @@ const addOrUpdateTask = () => {
     date: dateInput.value,
     description: descriptionInput.value,
   };
+
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   }
+};
+
+const updateTaskContainer = () => {
+  taskData.forEach(({ id, title, date, description }) => {
+    tasksContainer.innerHTML += `
+          <div class="task" id="${id}">
+            <p><strong>Title:</strong> ${title}</p>
+            <p><strong>Date:</strong> ${date}</p>
+            <p><strong>Description:</strong> ${description}</p>
+            <button type="button" class="btn">Edit</button>
+            <button type="button" class="btn">Delete</button>
+          </div>
+        `;
+  });
 };
 
 // function to clear the input fields
